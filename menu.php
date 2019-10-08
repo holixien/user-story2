7f59b3b5c3d68dbd20d4fc0481760f28c00169e5
holixien <holixien@gmail.com>
2019-10-08 03:18:19 +0200

    integration ecran 1

---
+++ menu.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GesTick</title>
+    <link rel="stylesheet" href="../public/css/menustyle.css" media="screen" type="text/css" />
+
+</head>
+<body>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+
+<div class="app-container">
+  
+  <nav class="nav">
+    <a href="#" class="nav__logo">
+      LOGO
+    </a>
+    <ul class="nav__list">
+      <li class="nav__list-item">
+        <a href="#" class="nav__item-link">Dashboard</a>
+      </li>
+      <li class="nav__list-item">
+        <a href="#" class="nav__item-link">Session</a>
+      </li>
+      <li class="nav__list-item">
+        <a href="#" class="nav__item-link">Utilisateur</a>
+      </li>
+      <li class="nav__list-item">
+        <a href="#" class="nav__item-link">Administration</a>
+      </li>
+      <li class="nav__list-item">
+        <a href="#" class="nav__item-link">Lorem</a>
+      </li>
+      <li class="nav__list-item">
+        <a href="#" class="nav__item-link">Lorem</a>
+      </li>
+      <li class="nav__list-item">
+        <a href="#" class="nav__item-link">Déconnexion</a>
+      </li>
+    </ul>
+  </nav>
+  
+  <div class="app-view">
+    
+    <nav class="utility-bar">
+      <div class="utility-bar__inner">
+        <a href="#" class="utility-bar__account">
+          <span class="utility-bar__account-avatar"></span>
+          <span class="utility-bar__account-name">Assistant Dir</span>
+        </a>
+      </div>
+    </nav>
+    
+    <main class="content">
+      <h1>Tableau de bord</h1>
+      
+    </main>
+    
+  </div>
+  
+</div>
+</body>
+</html>
